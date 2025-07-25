d5da9158e95f6471ff4753abb10a0b51f149f5f3
Jaira <jairesmejor@gmail.com>
2025-07-24 21:45:42 -0500

    cambie, para que el personaje rotara cada gradualmente cuando baja y cuando sube, tambien hice que el jugador apareciera en el centro del mapa

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,11 @@
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 
 public class Player {
     private int x, y;
     private int size = 20;
     private int speedY = 5;
+    private double tiltAngle = 0;
 
     public Player(int x, int y) {
         this.x = x;
@@ -12,25 +14,50 @@ public class Player {
 
     public void moveUp() {
         y -= speedY;
+        tiltAngle = -0.4;
     }
 
     public void moveDown() {
         y += speedY;
+        tiltAngle = 0.4;
     }
 
     public void update() {
+        x += 4;
         if (y < 0) y = 0;
         if (y > 580) y = 580;
+        tiltAngle *= 0.9;
     }
 
     public void draw(Graphics g) {
-        g.setColor(Color.CYAN);
-        int[] xPoints = {x, x + size, x};
-        int[] yPoints = {y, y + size / 2, y + size};
-        g.fillPolygon(xPoints, yPoints, 3);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.CYAN);
+
+        AffineTransform old = g2d.getTransform();
+
+
+        g2d.translate(100 + size / 2, y + size / 2);
+        g2d.rotate(tiltAngle);
+
+
+        int[] xPoints = {-size / 2, size / 2, -size / 2};
+        int[] yPoints = {-size / 2, 0, size / 2};
+
+        g2d.fillPolygon(xPoints, yPoints, 3);
+
+        g2d.setTransform(old);
     }
 
     public Rectangle getBounds() {
-        return new Rectangle(x, y, size, size);
+        return new Rectangle(100, y, size, size);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
+
